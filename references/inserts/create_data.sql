c572c506fc6623aab831b773fdc24b86fa2dca3d
Daniel Herrera <daniel.herrera2@teradata.com>
2023-05-19 16:40:53 +0200

    modified insert scripts

--- references/inserts/create_data.sql
+++ references/inserts/create_data.sql
@@ -1,6 +1,6 @@
 CREATE SET TABLE teddy_retailers.source_products AS
 (
-  SELECT id, name, category, price_cents
+  SELECT id, name, CAST(category AS CHAR(30)) as category, price_cents
     FROM (
 		LOCATION='/gs/storage.googleapis.com/clearscape_analytics_demo_data/DEMO_dbtAdvanced/raw_products.csv') as products
 ) WITH DATA;
@@ -12,7 +12,7 @@ CREATE SET TABLE teddy_retailers.source_customers AS
 ) WITH DATA;
 CREATE SET TABLE teddy_retailers.source_orders AS
 (
-  SELECT id, customer_id, order_date
+  SELECT id , customer_id, CAST(status AS CHAR(20)) as status, order_date
     FROM (
 		LOCATION='/gs/storage.googleapis.com/clearscape_analytics_demo_data/DEMO_dbtAdvanced/primary_orders_first.csv') as orders
 ) WITH DATA;
--- references/inserts/update_data.sql
+++ references/inserts/update_data.sql
@@ -1,14 +1,14 @@
 drop table teddy_retailers.source_orders;
 CREATE SET TABLE teddy_retailers.source_orders AS
 (
-  SELECT id, customer_id, order_date
+  SELECT id , customer_id, CAST(status AS CHAR(20)) as status, order_date
     FROM (
 		LOCATION='/gs/storage.googleapis.com/clearscape_analytics_demo_data/DEMO_dbtAdvanced/primary_orders_second.csv') as orders
 ) WITH DATA;
 drop table teddy_retailers.source_order_products;
 CREATE SET TABLE teddy_retailers.source_order_products AS
 (
-  SELECT order_id, product_id, product_quantity
+  SELECT  order_id, product_id, product_quantity
     FROM (
 		LOCATION='/gs/storage.googleapis.com/clearscape_analytics_demo_data/DEMO_dbtAdvanced/primary_order_products_second.csv') as orders_products
 ) WITH DATA;
