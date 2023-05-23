4ee06610273d9f83211f9c16955d3b7c64856a38
Daniel Herrera <daniel.herrera2@teradata.com>
2023-05-23 11:02:55 +0200

    fixed the consult customers query

--- references/query/consult_customers.sql
+++ references/query/consult_customers.sql
@@ -5,6 +5,6 @@ select
     sum(teddy_retailers.fct_order_details.amount) as tlv,
     max(teddy_retailers.fct_order_details.order_date) as last_ordered
 From teddy_retailers.dim_customers
-left join teddy_retailers.fct_order_details on teddy_retailers.fct_order_details = teddy_retailers.dim_customers.customer_id
+left join teddy_retailers.fct_order_details on teddy_retailers.fct_order_details.customer_id = teddy_retailers.dim_customers.customer_id
 GROUP BY 1, 2
 ORDER BY tlv DESC;
