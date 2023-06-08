517efc880591a14f41793436d6a7537a94825b46
Daniel Herrera <daniel.herrera2@teradata.com>
2023-06-08 11:03:49 +0200

    Update create_data.sql
    Fixed on the fly to support Angre

--- references/inserts/create_data.sql
+++ references/inserts/create_data.sql
@@ -15,7 +15,7 @@ CREATE MULTISET TABLE teddy_retailers.source_orders AS
   SELECT id , customer_id, CAST(status AS CHAR(20)) as status, order_date
     FROM (
 		LOCATION='/gs/storage.googleapis.com/clearscape_analytics_demo_data/DEMO_dbtAdvanced/primary_orders_first.csv') as orders
-) WITH DATA
+) WITH DATA;
 ALTER TABLE teddy_retailers.source_orders
   ADD order_duration PERIOD(TIMESTAMP(6) WITH TIME ZONE) NOT NULL
   AS TRANSACTIONTIME;
