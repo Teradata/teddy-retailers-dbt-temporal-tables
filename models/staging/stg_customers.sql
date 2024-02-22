04ae45404619b3e93ab25c92d6ef8bec790a4b24
Daniel Herrera <daniel.herrera2@teradata.com>
2024-02-22 14:00:54 +0100

    errorer branch

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -2,11 +2,10 @@
 	materialized="incremental",
 	on_schema_change="append_new_columns",
 	table_kind="MULTISET",
-    post_hook= [
-        "{% if not is_incremental() %} CREATE INDEX (email) ON {{ this }} {% endif %};",
-        "{% if not is_incremental() %} COLLECT STATISTICS INDEX (email), COLUMN ( customer_id ) ON {{ this }} {% endif %};"
-        ]
+	index="PRIMARY INDEX ( customer_id ) INDEX (email)",
+	post_hook= ["COLLECT STATISTICS INDEX (email), COLUMN ( id ) ON {{ this }};"]
 )}}
+
 
 with source as (
 
